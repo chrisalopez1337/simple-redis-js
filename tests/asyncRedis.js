ef3af6138363d6429e7b9cd554f7c5f56015ac7c
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-05 15:22:39 -0800

    Added tests to cover the Async based Redis client

---
+++ tests/asyncRedis.js
@@ -0,0 +1,43 @@
+const { AsyncRedis } = require('../lib/main.js');
+const { expect } = require('chai');
+const assert = require('assert');
+
+const asyncOptions = {
+    host: '127.0.0.1',
+    port: 6379,
+    keyPrefix: 'customKey-',
+};
+
+const rAsync = new AsyncRedis(asyncOptions);
+const key = 'test';
+const data = [ 1, 2, 3 ];
+const setexKey = 'test1';
+const ttl = 1000;
+
+describe('Async Based Redis Client', async () => {
+    it('Should accept dynamic options', () => {
+        const expectedKey = 'customKey-';
+        const actual = rAsync.options.keyPrefix;
+        expect(actual).to.equal(expectedKey);
+    });
+
+    it('Should be able to set data', async () => {
+        const res = await rAsync.set(key, data);
+        expect(res).to.equal(true);
+    });
+
+    it('Should be able to retrieve data', async () => {
+        const res = await rAsync.get(key);
+        assert.deepEqual(res, data);
+    });
+
+    it('Should be able to set data with an expiry', async () => {
+        const res = await rAsync.setex(setexKey, data, ttl);
+        expect(res).to.equal(true);
+    });
+
+    it('Should be able to clear data', async () => {
+        const res = await rAsync.flush();
+        expect(res).to.equal(true);
+    })
+});
